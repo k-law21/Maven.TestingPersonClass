4d1b55338d92b7c38ab29abd94cb6142ee83ca85
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-10.local>
2019-10-26 10:47:24 -0400

    Complete

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,44 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
-
+    private String race;
+    private String nationality;
+    private boolean athlete;
+    private String favSport;
+    private boolean onlyChild;
+    private int numSiblings;
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
-
     public Person(int age) {
+        this.age = 5;
     }
 
     public Person(String name) {
+        this.name = "Leon";
+
     }
 
     public Person(String name, int age) {
+        this.name = "Leon";
+        this.age = 5;
     }
 
+
     public void setName(String name) {
+        this.name = "Leon";
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
 }
